2b5a406b13eac83ad6d2910ac139247952a0d2d1
Chaerul Umam <chaerulumamb244@gmail.com>
2023-10-31 19:56:38 +0700

    build: Create API services.

---
+++ pages/api/businesses/[...slug].js
@@ -0,0 +1,9 @@
+import { getBusinessReview } from "../../../services";
+
+export default async function handler(req, res) {
+    const {slug, ...queries} = req.body
+    const [id] = slug
+    const data = await getBusinessReview(id, queries)
+
+    res.status(200).json({status: 200, data})
+}
